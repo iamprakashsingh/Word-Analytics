894e77ec2167f73411165cefb514f4da27ff485e
iamprakashsingh <prakashsingh30sep@gmail.com>
2024-10-25 23:18:32 +0530

    warning-made

--- src/TextArea.jsx
+++ src/TextArea.jsx
@@ -1,12 +1,26 @@
 import { useState } from "react";
+import Warning from "./Warning";
 
 export default function TextArea() {
   const [text,setText] = useState("");
+  const [warningText,setWarningText] = useState("");
   return (
+    <>
     <textarea className="textarea" value={text} onChange={(e)=>{
         const newText = e.target.value;
+        if(newText.includes('<script>')){
+          setWarningText("Scripts are not allowed");
+          return;
+        }
+        else if(newText.includes('@')){
+          setWarningText("Special characters are not allowed");
+          return;
+        }
+        setWarningText("");
         setText(newText);
 
     }} placeholder="Enter text here" spellCheck="false"/>
+      <Warning warningText={warningText}/>
+      </>
   )
 }
